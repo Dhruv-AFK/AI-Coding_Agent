f27f70175cfb8971e9e8afc0288a50e037536eb4
Dhruv-AFK <dhruvjha1239@gmail.com>
2026-06-06 21:33:46 +0530

    Initial commit: Complete single-file PDF watermark tool

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from gemini-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="""
+    Why are Boot.dev and FreeCodeCamp such great places to learn backend development? 
+    Use one paragraph maximum.
+    """,
+)
+print(response.text)
+print(f"Prompt tokens : {response.usage_metadata.prompt_tokens_count}")
+print(f"Response tokens : {response.usage_metadata.candidates_tokens_count}")
